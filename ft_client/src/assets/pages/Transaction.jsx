0e19377d922419c686b79a4c6c337e19e6f51b8e
nawang tashi lama <binayamoktan26@gmail.com>
2026-04-10 23:51:39 +0545

    structure of transaction part

--- ft_client/src/assets/pages/Transaction.jsx
+++ ft_client/src/assets/pages/Transaction.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { TransactionForm } from "../../../components/TransactionForm";
+import { TransactionTable } from "../../../components/TransactionTable";
 
 const Transaction = () => {
   return (
     <Container className="p-5">
       <Row className=" bg-dark p-5 rounded">
         {/* starting transaction  */}
-        <Col md={6}>Todo transaction</Col>
+      
+        <Col >  
+        <TransactionForm />
+        <hr />
+
+        <TransactionTable />
+        </Col>
       </Row>
     </Container>
   );
