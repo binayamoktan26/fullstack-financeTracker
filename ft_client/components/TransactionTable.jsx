72b5e91f9eea0d8cf95d1925db383c72fe5a6e03
nawang tashi lama <binayamoktan26@gmail.com>
2026-05-15 23:30:32 +0545

    displaying data in the table

--- ft_client/components/TransactionTable.jsx
+++ ft_client/components/TransactionTable.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import { useUser } from '../src/context/UserContex';
 export const TransactionTable = () => {
+  const{transaction}  =useUser()
+  console.log(transaction)
   return (
     <Table striped bordered hover>
       <thead>
@@ -13,20 +16,23 @@ export const TransactionTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
+        {transaction.lengeth > 0 && transaction.map((t,i)=>(
+   <tr key={t._id}>
           <td>1</td>
           <td>2023-10-01</td>
           <td>Salary</td>
           <td></td>
           <td>rs 22222</td>
         </tr>
-        <tr>
+        ) ) }
+      
+        {/* <tr>
           <td>2</td>
           <td>2023-10-02</td>
           <td>shopping</td>
           <td>Rs.12000</td>
           <td></td>
-        </tr>
+        </tr> */}
         <tr className='fw-bold text-end'>
           
           <td colSpan={3}>Total</td>
