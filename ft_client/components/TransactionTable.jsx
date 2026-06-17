e54e2b5df52e1b33abfc57564e8d06351c37fdc6
nawang tashi lama <binayamoktan26@gmail.com>
2026-06-17 23:08:39 +0545

    mobile responsive for transaction table

--- ft_client/components/TransactionTable.jsx
+++ ft_client/components/TransactionTable.jsx
@@ -10,15 +10,13 @@ import { toast } from "react-toastify";
 export const TransactionTable = () => {
   const [displayTransaction, setDisplayTransaction] = useState([]);
 
-  const { transaction, toggleModal,getTransaction } = useUser();
+  const { transaction, toggleModal, getTransaction } = useUser();
   const [idsToDelete, setIdsToDelete] = useState([]);
 
   useEffect(() => {
     setDisplayTransaction(transaction);
   }, [transaction]);
-  useEffect(() => {
-  
-  }, [idsToDelete]);
+  useEffect(() => {}, [idsToDelete]);
 
   const balance = displayTransaction.reduce((acc, t) => {
     return t.type === "income" ? acc + t.amount : acc - t.amount;
@@ -26,7 +24,7 @@ export const TransactionTable = () => {
 
   const handleOnSearch = (e) => {
     const { value } = e.target;
-  
+
     const filteredAvg = transaction.filter(({ title }) => {
       return title.toLowerCase().includes(value.toLowerCase());
     });
@@ -52,7 +50,6 @@ export const TransactionTable = () => {
   };
   const handleOnDelete = async () => {
     if (confirm(`Are you sure to delete ${idsToDelete.length} transactions?`)) {
-
       // const {status , message} = await deleteTransaction(idsToDelete);
       const pending = deleteTransaction(idsToDelete);
       toast.promise(pending, {
@@ -61,22 +58,23 @@ export const TransactionTable = () => {
       const { status, message } = await pending;
 
       toast[status](message);
-      status === "success" && getTransaction() && setIdsToDelete([])
+      status === "success" && getTransaction() && setIdsToDelete([]);
     }
   };
 
   return (
     <div>
-      <div className="d-flex justify-content-between pt-2 mb-2">
+      <div className="d-flex flex-column flex-md-row justify-content-between gap-2 pt-2 mb-2">
         <div>{displayTransaction.length} Transaction found ! </div>
         <div>
           <Form.Control
+            className="w-100"
             type="text"
             placeholder="Search transactions..."
             onChange={handleOnSearch}
           ></Form.Control>
         </div>
-        <div>
+        <div className="text-center">
           <Button variant="primary" onClick={() => toggleModal(true)}>
             <BsClipboardPlus /> Add new Transaction
           </Button>
@@ -90,54 +88,60 @@ export const TransactionTable = () => {
           checked={idsToDelete.length === displayTransaction.length}
         />
       </div>
-      <Table striped hover>
-        <thead>
-          <tr>
-            <th>S.N</th>
-            <th>Date</th>
-            <th>Title</th>
-            <th>Dr.</th>
-            <th>Cr.</th>
-          </tr>
-        </thead>
-        <tbody>
-          {displayTransaction.length > 0 &&
-            displayTransaction.map((t, i) => (
-              <tr key={t._id}>
-                <td>{i + 1}</td>
-                <td>
-                  <Form.Check
-                    label={t.createdAt.slice(0, 10)}
-                    value={t._id}
-                    onChange={handleOnSelect}
-                    checked={idsToDelete.includes(t._id)}
-                  />
-                </td>
-                <td>{t.title}</td>
-                {t.type === "expenses" && (
-                  <>
-                    <td className="dr">{t.amount}</td>
-                    <td></td>
-                  </>
-                )}
-                {t.type === "income" && (
-                  <>
-                    <td></td>
-                    <td className="cr">{t.amount}</td>
-                  </>
-                )}
-              </tr>
-            ))}
+      <div className="table-responsive">
+        <Table striped hover responsive>
+          <thead>
+            <tr>
+              <th>S.N</th>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Dr.</th>
+              <th>Cr.</th>
+            </tr>
+          </thead>
+          <tbody>
+            {displayTransaction.length > 0 &&
+              displayTransaction.map((t, i) => (
+                <tr key={t._id}>
+                  <td>{i + 1}</td>
+                  <td>
+                    <Form.Check
+                      label={t.createdAt.slice(0, 10)}
+                      value={t._id}
+                      onChange={handleOnSelect}
+                      checked={idsToDelete.includes(t._id)}
+                    />
+                  </td>
+                  <td>{t.title}</td>
+                  {t.type === "expenses" && (
+                    <>
+                      <td className="dr">{t.amount}</td>
+                      <td></td>
+                    </>
+                  )}
+                  {t.type === "income" && (
+                    <>
+                      <td></td>
+                      <td className="cr">{t.amount}</td>
+                    </>
+                  )}
+                </tr>
+              ))}
 
-          <tr className="fw-bold text-end">
-            <td colSpan={3}>Total</td>
-            <td colSpan={2}>{balance}</td>
-          </tr>
-        </tbody>
-      </Table>
+            <tr className="fw-bold text-end">
+              <td colSpan={3}>Total</td>
+              <td colSpan={2}>{balance}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
       {idsToDelete.length > 0 && (
         <div className="d-grid">
-          <Button variant="danger" onClick={handleOnDelete}>
+          <Button
+            className="w-100 w-md-auto"
+            variant="danger"
+            onClick={handleOnDelete}
+          >
             Delete {idsToDelete.length} Transactions
           </Button>
         </div>
